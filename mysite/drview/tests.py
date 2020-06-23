fa2c1a70fc4edbd404f126427c8616506df35ff7
jazimmerm <jaz.zimms@gmail.com>
2020-06-23 18:17:03 -0400

    Latest working version - added appt form error messages - added support for recurring events with rrule

--- mysite/drview/tests.py
+++ mysite/drview/tests.py
@@ -1,18 +1,4 @@
-from django.contrib.auth.models import User
-from drview.models import Doctor, Specialty, Patient, Appointments
-import datetime
 
-# for creating user instances must use Doctor.objects.create(user=USERINSTANCE)
-
-momdoc = Doctor.objects.get(name='Cindy')
-pat = Patient.objects.get(name='pat')
-
-st = datetime.datetime(2020, 5, 15,  14, 0, 0, 0)
-et = datetime.datetime(2020, 5, 15,  16, 0, 0, 0)
-
-appt1 = Appointments.objects.create(starts_at = st, ends_at = et, appt_location = 'West Palm', appt_doctor=momdoc, appt_patient=pat, reason='Legs')
-
-appt1.save()
 
 
 
